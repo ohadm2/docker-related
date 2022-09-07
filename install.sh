6a063f376ba4ea9ed69aa4bccf48670e354cd3ea
Ohad Malichi <ohad.malichi@gmail.com>
2022-09-07 09:06:57 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cp ~/.bashrc ~/.bashrc-$(date +"%s")
+cp ~/.bashrc ~/bashrc-$(date +"%s")
 
 curl -o ~/.bashrc https://raw.githubusercontent.com/ohadm2/docker-related/main/docker-bashrc-25-5-22
